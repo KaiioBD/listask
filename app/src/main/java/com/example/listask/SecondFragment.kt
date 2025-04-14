ec77663343c856bc46abdf0beccc78d925dd9937
Kalil <kalilvg9@gmail.com>
2025-04-14 17:48:22 -0600

    Modificaciones

---
+++ app/src/main/java/com/example/listask/SecondFragment.kt
@@ -0,0 +1,43 @@
+package com.example.listask
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.listask.databinding.FragmentRegisterBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        setupView()
+        return binding.root
+
+    }
+
+    private fun setupView() {
+        binding.iconButton.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
