c85a6eaad8b42e8d028f633527dba5f879664209
KaiioBD <kalilvg9@gmail.com>
2025-06-12 00:55:39 -0600

    Implementacion de manejo de errores

--- app/src/main/java/com/example/listask/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/listask/viewModel/LoginViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.listask.core.ResultWrapper
+import com.example.listask.network.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class LoginViewModel : ViewModel() {
+
+    private val repository = UserRepository()
 
     private val _loaderState = MutableLiveData<Boolean>()
     val loaderState: LiveData<Boolean>
@@ -22,12 +26,15 @@ class LoginViewModel : ViewModel() {
     fun requestSignIn(email: String, password: String) {
         _loaderState.value = true
         viewModelScope.launch {
-            val result = firebase.signInWithEmailAndPassword(email, password).await()
-            _loaderState.value = false
-            result.user?.let {
+            when (val result = repository.login(email, password)) {
+                    is ResultWrapper.Success -> {
+                _loaderState.value = false
                 _sessionValid.value = true
-            } ?: run {
-                Log.i("Firebase", "Ocurrio un problema")
+            }
+                is ResultWrapper.Error -> {
+                    _loaderState.value = false
+                    val errorMessage = result.exception.message
+                }
             }
         }
     }
--- app/src/main/java/com/example/listask/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/listask/viewModel/RegisterViewModel.kt
@@ -5,37 +5,41 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.listask.core.ResultWrapper
+import com.example.listask.network.UserRepository
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class RegisterViewModel: ViewModel() {
 
+    private val repository = UserRepository()
+
     private val _loaderState = MutableLiveData<Boolean>()
     val loaderState: LiveData<Boolean> get() = _loaderState
+
+    private val _createdUser = MutableLiveData<Boolean>()
+    val createdUser: LiveData<Boolean> get() = _createdUser
 
     private val _signUpSuccess = MutableLiveData<Boolean>()
     val signUpSuccess: LiveData<Boolean> get() = _signUpSuccess
 
-    private val firebase = FirebaseAuth.getInstance()
+    //private val firebase = FirebaseAuth.getInstance()
 
     fun requestSignUp(email: String, password: String) {
         _loaderState.value = true
+        _signUpSuccess.value = false
         viewModelScope.launch {
-            try {
-                val result = firebase.createUserWithEmailAndPassword(email, password).await()
+            when(val result = repository.register(email, password)) {
+                    is ResultWrapper.Success -> {
                 _loaderState.value = false
-                result.user?.let {
-                    Log.i("Firebase", "Se pudo crear el usuario")
-                    _signUpSuccess.value = true
-                } ?: run {
-                    Log.e("Firebase", "El usuario es null")
-                    _signUpSuccess.value = false
+                _createdUser.value = true
+                _signUpSuccess.value = true
+            }
+                is ResultWrapper.Error -> {
+                    _loaderState.value = false
+                    val errorMessage = result.exception.message
                 }
-            } catch (e: Exception) {
-                Log.e("Firebase", "Error al crear el usuario", e)
-                _loaderState.value = false
-                _signUpSuccess.value = false
             }
         }
     }
